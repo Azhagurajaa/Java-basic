a2bbd3fa3e6cc02281696068eca52b3ae986f6a6
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-09-19 02:13:42 -0700

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+class DataTypes
+	{
+		public static void main(String args[])
+		{
+			int myNum = 5;               // Integer (whole number)
+			float myFloatNum = 5.99f;    // Floating point number
+			char myLetter = 'D';         // Character
+			boolean myBool = true;       // Boolean
+			String myText = "Hello";     // String
+
+			System.out.println("print my Number :"+myNum);
+			System.out.println("print my Float :"+myFloatNum);
+			System.out.println("print my Letter :"+myLetter);
+			System.out.println("print my Boolean :"+myBool);
+			System.out.println("print my Text :"+myText);
+
+		}
+	}
