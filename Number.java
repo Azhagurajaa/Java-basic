7a29f86a4c9cec1c188bd57773e36a2195c31534
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-09-19 02:23:50 -0700

    DataType Number

---
+++ Number.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+ class Number
+ {
+	 public static void main(String args[])
+	 {
+		int   i = 100000;
+	    float f1 = 35e3f;
+		double d1 = 12E4d;
+		
+		
+		System.out.println("integer Number is :" + i);
+		System.out.println("float Number is :" + f1);
+		System.out.println("double Number is :" + d1);
+	 }
+ }
