ef168404cc7e49ca68e786372dba17fe7608f2d5
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-09-19 01:49:12 -0700

    Add files via upload

---
+++ command.java
@@ -0,0 +1,10 @@
+// 02 command line Arguments in java 
+import java.lang.*;
+class command{
+	public static void main(String args[]){
+		for(int i=0; i<args.length; i++)
+		{
+			System.out.println(args[i]);
+		}
+	}
+}
---
+++ variables.java
@@ -0,0 +1,16 @@
+// 04 variables
+class variables{
+	public static void main(String args[]){
+		String name = "Azhagu Raja";
+		int Age = 25;
+		char genter = 'm';
+		float percent = 98.5f;
+		boolean merried = false;
+		
+		System.out.println("Name : "+name);
+		System.out.println("Age : "+Age);
+	    System.out.println("Genter : "+genter);
+	    System.out.println("Percent : "+percent);
+		System.out.println("Merried : "+merried);
+	}
+}
