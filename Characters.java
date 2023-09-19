4bbdc9f1795536c7e457cc05838827240518f771
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-09-19 02:48:33 -0700

    Primitive data types
    The char data type is used to store a single character.

---
+++ Characters.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+ class Characters
+ {
+
+	 public static void main(String args[])
+	 {
+		 //The char data type is used to store a single character.
+		 char myGrade = 'A';
+         System.out.println("myGrade :" + myGrade);
+		 
+		 //Alternatively, if you are familiar with ASCII values
+		 char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+		 System.out.println("ASCII values 65 :" + myVar1);
+		 System.out.println("ASCII values 66 :" + myVar2);
+		 System.out.println("ASCII values 67 :" + myVar3);
+	 }
+ }
