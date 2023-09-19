5014e3d30e0bb1f821f6dbb4c6f354bc93f34984
Azhagurajaa <89646803+Azhagurajaa@users.noreply.github.com>
2023-09-19 02:32:17 -0700

    Primitive data types
    Boolean values are mostly used for conditional testing.

---
+++ Boolean.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+ class Boolean
+ {
+	 public static void main(String args[])
+	 {
+		boolean java1 = true;
+		boolean java2= false;
+		System.out.println(java1);     // Outputs true
+		System.out.println(java2);    // Outputs false
+		
+		//Boolean values are mostly used for conditional testing.
+	 }
+ }
